4320a838504efa65ad7f9a83488bb102b7ac0ec3
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-07 16:10:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            double withReminderCourses = (double)people / capacity;
+
+            Console.WriteLine(Math.Ceiling(withReminderCourses));
+           
+        }
+    }
+}
